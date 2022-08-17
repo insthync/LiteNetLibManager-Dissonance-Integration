7a45aa3f5c332ac1a073ca342ebfb9f3778f4969
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-08-18 04:11:17 +0700

    Add `IsOwnerClient` to `ILnlMPlayer` interface

--- ILnlMPlayer.cs
+++ ILnlMPlayer.cs
@@ -5,6 +5,7 @@ namespace Dissonance.Integrations.LiteNetLibManager
     public interface ILnlMPlayer
     {
         public long ConnectionId { get; }
+        public bool IsOwnerClient { get; }
         public Vector3 Position { get; }
         public Quaternion Rotation { get; }
     }
